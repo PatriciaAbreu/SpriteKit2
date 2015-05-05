7d0ce3b67dffed78b48ed6449424b16cbdd54f7b
PatriciaAbreu <PatriciaAbreu@MacBook-Pro-de-Patricia.local>
2015-05-05 17:27:07 -0300

    Game Funcionando!

--- Teste2_SpriteKit/Teste2_SpriteKit/GameViewController.swift
+++ Teste2_SpriteKit/Teste2_SpriteKit/GameViewController.swift
@@ -16,7 +16,7 @@ extension SKNode {
             var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
             
             archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! SKScene
             archiver.finishDecoding()
             return scene
         } else {
@@ -30,14 +30,14 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+        if let scene = GameOverScene.unarchiveFromFile("GameOverScene") as? GameOverScene {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
+            skView.ignoresSiblingOrder = false
             
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
